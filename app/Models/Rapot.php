9eaf5ab82c12b5a95fafe994cae83c809ccf1c83
adiwarsa <adiwarsa03@gmail.com>
2023-09-02 20:03:06 +0800

    Adding Rapot Model

---
+++ app/Models/Rapot.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Rapot extends Model
+{
+    use HasFactory;
+    protected $table = 'rapot';
+    protected $fillable = [
+        'santri_id',
+        'guru_id',
+        'kelas_id', 
+        'mapel_id',
+        'nilai_ganjil', 
+        'nilai_genap', 
+        'predikat_genap', 
+        'predikat_ganjil'
+    ];
+}
