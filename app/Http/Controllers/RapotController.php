6a384cda405ef9de80422b8aea5d0b04724ae0d4
adiwarsa <adiwarsa03@gmail.com>
2023-09-16 11:29:09 +0800

    add function create store rapot

--- app/Http/Controllers/RapotController.php
+++ app/Http/Controllers/RapotController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Models\Kelas;
+use App\Models\MataPelajaran;
+use App\Models\NilaiRapot;
 use App\Models\Rapot;
 use App\Models\Santri;
 use App\Models\User;
@@ -22,9 +24,30 @@ class RapotController extends Controller
         return view('rapot.index', $data);
     }
 
-    public function rapotsantri()
+    public function listSantri($kelasId)
     {
+        $data['pageTitle'] = 'Santri';
+        // Retrieve the selected class
+        $selectedClass = Kelas::findOrFail($kelasId);
 
+        // Retrieve a list of students in the selected class
+        $data['santri'] = Santri::where('id_kelas', $selectedClass->id)->get();
+
+        $data['namakelas'] = $selectedClass->kelas;
+        $data['kelasId'] = $selectedClass->id;
+        return view('rapot.listsantri', $data);
+    }
+
+    public function createRapot($kelasId, $santriId)
+    {
+        $data['pageTitle'] = 'Create Rapot Santri';
+        // Retrieve the selected class, student, and associated subjects
+        $selectedClass = Kelas::findOrFail($kelasId);
+
+        $data['wali'] = $selectedClass->wali_id;
+        $data['santri'] = Santri::findOrFail($santriId);
+        $data['mapels'] = MataPelajaran::where('id_kelas', $selectedClass->id)->get();
+        return view('rapot.create', $data);
     }
 
     /**
@@ -32,15 +55,64 @@ class RapotController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function create(Request $request)
+    public function storeRapot(Request $request)
     {
-        $pageTitle = 'Create Rapot Santri';
-        // Retrieve the Santri for whom you want to create Rapot
-        $santri = Santri::find($request->id);
-        // Retrieve the subjects associated with the Santri's class
-        $mapel = $santri->kelas->mapels;
-        @dd($mapel);
-        return view('rapot.index', compact('santri', 'mapel', 'pageTitle'));
+        $request->validate([
+            'santri_id' => 'required',
+            'mapel_ids' => 'required|array',
+            'nilaipengetahuans' => 'required|array',
+            'nilaiketerampilans' => 'required|array',
+            'predikatpengetahuans' => 'required|array',
+            'predikatketerampilans' => 'required|array',
+        ]);
+    
+        // Create a new Rapot
+        $rapot = Rapot::create([
+            'santri_id' => $request->santri_id,
+            'wali_id' => $request->wali_id,
+            'kelas_id' => $request->kelas_id,
+            'semester' => 1,
+        ]);
+       
+        // Loop through each mapel and create associated nilaiRapot records
+        foreach ($request->mapel_ids as $index => $mapel_id) {
+            // Calculate predikat based on nilai
+            $nilaiPengetahuan = $request->nilaipengetahuans[$index];
+            $nilaiKeterampilan = $request->nilaiketerampilans[$index];
+        
+            $predikatPengetahuan = '';
+            $predikatKeterampilan = '';
+        
+            if ($nilaiPengetahuan >= 80) {
+                $predikatPengetahuan = 'A';
+            } elseif ($nilaiPengetahuan >= 70) {
+                $predikatPengetahuan = 'B';
+            } elseif ($nilaiPengetahuan >= 60) {
+                $predikatPengetahuan = 'C';
+            } else {
+                $predikatPengetahuan = 'D';
+            }
+        
+            if ($nilaiKeterampilan >= 80) {
+                $predikatKeterampilan = 'A';
+            } elseif ($nilaiKeterampilan >= 70) {
+                $predikatKeterampilan = 'B';
+            } elseif ($nilaiKeterampilan >= 60) {
+                $predikatKeterampilan = 'C';
+            } else {
+                $predikatKeterampilan = 'D';
+            }
+        
+            NilaiRapot::create([
+                'rapot_id' => $rapot->id,
+                'guru_id' => $request->wali_id,
+                'mapel_id' => $mapel_id,
+                'nilaipengetahuan' => $nilaiPengetahuan,
+                'nilaiketerampilan' => $nilaiKeterampilan,
+                'predikatpengetahuan' => $predikatPengetahuan,
+                'predikatketerampilan' => $predikatKeterampilan,
+            ]);
+        }
     }
 
     /**
