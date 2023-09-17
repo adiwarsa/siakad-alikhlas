05e198d2053daffb34e59652c725f63dd8702989
adiwarsa <adiwarsa03@gmail.com>
2023-09-17 12:51:08 +0800

    modify the rapotsantri blade

--- resources/views/rapot/rapotsantri.blade.php
+++ resources/views/rapot/rapotsantri.blade.php
@@ -7,11 +7,19 @@
     <link rel="stylesheet" href="{{ asset('assets/modules/datatables/Select-1.2.4/css/select.bootstrap4.min.css') }}">
 @endsection
 @section('content')
+<section class="section">
+    <div class="section-header">
+        <h1>{{ $pageTitle }} {{ $rapot->kelas->kelas }}</h1>
+        <div class="section-header-breadcrumb">
+            <div class="breadcrumb-item"><a href="{{ route('rapot.index')}}">Rapot Santri </a></div>
+            <div class="breadcrumb-item active">Detail Rapot Santri</div>
+        </div>
+    </div>
 <div class="col-md-12">
     <!-- general form elements -->
     <div class="card card-primary">
       <div class="card-header">
-        <h3 class="card-title">Detail Rapot</h3>
+        <h4 class="card-title">Detail Rapot</h4>
       </div>
       <!-- /.card-header -->
         <div class="card-body">
@@ -19,33 +27,30 @@
             <div class="col-md-12">
                 <table class="table" style="margin-top: -10px;">
                     <tr>
-                        <td>No Induk Siswa</td>
+                        <td>No Induk Santri</td>
                         <td>:</td>
-                        <td>2</td>
+                        <td>{{ $rapot->santri->nis }}</td>
                     </tr>
                     <tr>
                         <td>Nama Siswa</td>
                         <td>:</td>
-                        <td>Kurcace</td>
+                        <td>{{ $rapot->santri->nama }}</td>
                     </tr>
                     <tr>
                         <td>Nama Kelas</td>
                         <td>:</td>
-                        <td>rawr</td>
+                        <td>{{ $rapot->kelas->kelas }} {{ $rapot->kelas->madrasah }}</td>
                     </tr>
                     <tr>
                         <td>Wali Kelas</td>
                         <td>:</td>
-                        <td>rawr</td>
+                        <td>{{ $rapot->guruwali->name }}</td>
                     </tr>
                     <tr>
                         <td>Semester</td>
                         <td>:</td>
                         <td>
-                           
-                                Semester Ganjil
-                           
-
+                            {{ $rapot->semester }}
                         </td>
                     </tr>
                     <tr>
@@ -77,22 +82,24 @@
                         </tr>
                     </thead>
                     <tbody>
-                         
-                                <tr>
-                                    <td>1</td>
-                                    <td>tarar</td>
-                                   
-
-                                    <td class="ctr">d</td>
-                                    <td class="ctr">3</td>
-                                    <td class="ctr">w</td>
-                                    <td class="ctr">f</td>
-                                    <td class="ctr">g</td>
-                                    <td class="ctr">h</td>
-                                </tr>
-
-                    </tbody>
+                    @forelse($rapot->nilaiRapots as $index => $nilairapot)
+                        <tr>
+                            <td>{{ $index + 1 }}</td>
+                            <td>{{ $nilairapot->mapel->nama }}</td>
+                            <td class="ctr">{{ $nilairapot->nilaipengetahuan }}</td>
+                            <td class="ctr">{{ $nilairapot->predikatpengetahuan }}</td>
+                            <td class="ctr">{{ $nilairapot->getDeskripsiAttribute() }}</td> <!-- Use the getDeskripsiAttribute() method -->
+                            <td class="ctr">{{ $nilairapot->nilaiketerampilan }}</td>
+                            <td class="ctr">{{ $nilairapot->predikatketerampilan }}</td>
+                            <td class="ctr">{{ $nilairapot->getDeskripsiAttribute() }}</td> <!-- Use the getDeskripsiAttribute() method -->
+                        </tr>
+                    @empty
+                        <td>
+                            No Data Rapot
+                        </td>
+                    @endforelse
                 </table>
+                
             </div>
           </div>
         </div>
@@ -100,6 +107,7 @@
     </div>
     <!-- /.card -->
 </div>
+</section>
 <style>
     .ctr {
             text-align: center !important;
