4746a8fd644f79fd8886be96b3c8e1435e775974
adiwarsa <adiwarsa03@gmail.com>
2023-09-18 01:44:48 +0800

    add sidebar for role orangtua and guru

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -8,6 +8,14 @@
                     <ul class="sidebar-menu">
                         <li class="menu-header">Dashboard</li>
                         <li class="{{ Request::is('dashboard') ? 'active' : '' }}"><a class="nav-link" href="{{ url('/') }}"><i class="fas fa-fire"></i> <span>Dashboard</span></a></li>
+                        @role('orangtua')
+                        <li class="{{ Request::is('rapot*') ? 'active' : '' }}"><a class="nav-link" href="{{ route('ortu.santri') }}"><i class="fas fa-book"></i> <span>Rapot</span></a></li>
+                        @endrole
+                        @role('guru')
+                            <li class="{{ Request::is('santri*') ? 'active' : '' }}"><a class="nav-link" href="{{ url('/santri') }}"><i class="fas fa-user"></i> <span>Santri</span></a></li>
+                            <li class="{{ Request::is('jadwal*') ? 'active' : '' }}"><a class="nav-link" href="{{ url('/jadwal') }}"><i class="fas fa-book"></i> <span>Jadwal Pelajaran</span></a></li>
+                            <li class="{{ Request::is('rapot*') ? 'active' : '' }}"><a class="nav-link" href="{{ url('/rapot') }}"><i class="fas fa-book"></i> <span>Rapot</span></a></li>
+                        @endrole
                         @role('administrator')
                             <li class="menu-header"><i>Master</i></li>
                             <li class="{{ Request::is('users*') ? 'active' : '' }}"><a class="nav-link" href="{{ url('/users') }}"><i class="fas fa-users-cog"></i> <span>Users</span></a></li>
@@ -20,5 +28,6 @@
                             <li class="{{ Request::is('permissions*') ? 'active' : '' }}"><a class="nav-link" href="{{ url('/permissions') }}"><i class="fas fa-key"></i> <span>Permissions</span></a></li>
                             <li class="{{ Request::is('roles*') ? 'active' : '' }}"><a class="nav-link" href="{{ url('/roles') }}"><i class="fas fa-user-tag"></i> <span>Roles</span></a></li>
                         @endrole
+
                     </ul>
                 </aside>
