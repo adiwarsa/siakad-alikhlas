835732a8795bdf117a3ea090d9f494e9f72f0e16
adiwarsa <adiwarsa03@gmail.com>
2023-09-18 01:45:31 +0800

    make ortu crud

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Santri;
 use App\Models\User;
+use App\Models\UserDetail;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Spatie\Permission\Models\Role;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Validation\Rule;
 
 class UserController extends Controller
 {
@@ -45,6 +48,158 @@ class UserController extends Controller
         return view('users.create', $data);
     }
 
+    public function createortu()
+    {
+        $data['pageTitle'] = 'Tambah Data Orang Tua';
+        $data['santri'] = Santri::all();
+
+        return view('users.createortu', $data);
+    }
+
+    public function storeortu(Request $request)
+    {
+        $rules = [
+            'name' => 'required',
+            'email' => ['required', 'string', 'email', 'unique:users'],
+            'username' => ['required', 'string', 'max:20', 'unique:users'],
+            'password' => ['required', 'string', 'confirmed'],
+            'nama_lengkap' => 'required',
+            'nohp' => 'required',
+            'noinduk' => 'required',
+            'alamat' => 'required',
+            'tempat_lahir' => 'required', 
+            'tanggal_lahir' => 'required',
+            'santri_id' => 'required',
+        ];
+
+        $customMessages = [
+            'name.required' => 'Nama belum diisi!',
+            'email.required' => 'Email belum diisi!',
+            'email.unique' => 'Email telah digunakan!',
+            'username.required' => 'Nama pengguna belum diisi!',
+            'username.unique' => 'Nama pengguna telah digunakan!',
+            'password.required' => 'Kata sandi belum diisi!',
+            'password.confirmed' => 'Kata sandi tidak cocok!',
+            'tempat_lahir.required' => 'Field tempat lahir belum diisi!',
+            'tanggal_lahir.required' => 'Field tanggal lahir belum diisi!',
+            'nama_lengkap' => 'Field nama lengkap belum diisi!',
+            'nohp' => 'Field nomor telp belum diisi!',
+            'noinduk'=> 'Field nomor induk belum diisi!',
+            'alamat' => 'Field alamat belum diisi!',
+            'santri_id' => 'Santri belum dipilih!',
+            
+        ];
+
+        $this->validate($request, $rules, $customMessages);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'username' => $request->username,
+            'password' => Hash::make($request->password),
+            'email_verified_at' => Carbon::now()
+        ]);
+        // Find or create the "administrator" role
+        $role = Role::firstOrCreate(['name' => 'orangtua']);
+
+        // Assign the role to the user
+        $user->assignRole($role);
+
+        $userDetail = UserDetail::create([
+            'user_id' => $user->id, // Assign the user_id to the user's ID
+            'noinduk' => $request->noinduk,
+            'nama_lengkap' => $request->nama_lengkap,
+            'nohp' => $request->nohp,
+            'alamat' => $request->alamat,
+            'tempat_lahir' => $request->tempat_lahir,
+            'tanggal_lahir' => $request->tanggal_lahir,
+            'santri_id' => $request->santri_id,
+        ]);
+
+        $user->id_detail = $userDetail->id;
+        $user->save();
+        
+
+        return redirect('/users')->with('message', 'Data Orang Tua telah ditambahkan'); 
+    }
+
+    public function editortu($id)
+    {
+        $data['pageTitle'] = 'Ubah Data Guru';
+        $data['user'] = User::where('id', $id)->first();
+        $data['santri'] = Santri::all();
+
+        return view('users.editortu', $data);
+    }
+
+    public function updateortu(Request $request, $id)
+    {
+        // Validate the request data, similar to the store method
+        $rules = [
+            'name' => 'required',
+            'email' => ['required', 'string', 'email', Rule::unique('users')->ignore($id)],
+            'username' => ['required', 'string', 'max:20', Rule::unique('users')->ignore($id)],
+            'password' => ['nullable', 'string', 'confirmed'],
+            'nama_lengkap' => 'required',
+            'nohp' => 'required',
+            'noinduk' => 'required',
+            'alamat' => 'required',
+            'tempat_lahir' => 'required', 
+            'tanggal_lahir' => 'required',
+            'santri_id' => 'required',
+        ];
+
+        $customMessages = [
+            'name.required' => 'Nama belum diisi!',
+            'email.required' => 'Email belum diisi!',
+            'email.unique' => 'Email telah digunakan!',
+            'username.required' => 'Nama pengguna belum diisi!',
+            'username.unique' => 'Nama pengguna telah digunakan!',
+            'password.required' => 'Kata sandi belum diisi!',
+            'password.confirmed' => 'Kata sandi tidak cocok!',
+            'tempat_lahir.required' => 'Field tempat lahir belum diisi!',
+            'tanggal_lahir.required' => 'Field tanggal lahir belum diisi!',
+            'nama_lengkap' => 'Field nama lengkap belum diisi!',
+            'nohp' => 'Field nomor telp belum diisi!',
+            'noinduk'=> 'Field nomor induk belum diisi!',
+            'alamat' => 'Field alamat belum diisi!',
+            'santri_id' => 'Santri belum dipilih!',
+            
+        ];
+
+        $this->validate($request, $rules, $customMessages);
+
+        // Find the user by their ID
+        $user = User::findOrFail($id);
+
+        // Update the user data
+        $user->update([
+            'name' => $request->name,
+            'email' => $request->email,
+            'username' => $request->username,
+            'email_verified_at' => Carbon::now(),
+        ]);
+
+        // Update the password if it's provided
+        if ($request->filled('password')) {
+            $user->update([
+                'password' => Hash::make($request->password),
+            ]);
+        }
+
+        // Update the associated user detail
+            $user->userDetail->update([
+                'noinduk' => $request->noinduk,
+                'nama_lengkap' => $request->nama_lengkap,
+                'nohp' => $request->nohp,
+                'alamat' => $request->alamat,
+                'tempat_lahir' => $request->tempat_lahir,
+                'tanggal_lahir' => $request->tanggal_lahir,
+                'santri_id' => $request->santri_id,
+            ]);
+
+        return redirect('/users')->with('message', 'Data Orang Tua telah diperbarui');
+    }
     /**
      * Store a newly created resource in storage.
      *
