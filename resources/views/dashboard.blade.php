5f032e0d43bfa8c0338bf930540a6e63433d00e7
adiwarsa <adiwarsa03@gmail.com>
2023-10-01 14:47:50 +0800

    dashboard edit

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -22,8 +22,139 @@
         @endif
 
         <div class="section-body">
+        <section>
+            <div class="row">
+                <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+                  <div class="card card-statistic-1">
+                    <div class="card-icon bg-primary">
+                      <i class="far fa-user"></i>
+                    </div>
+                    <div class="card-wrap">
+                      <div class="card-header">
+                        <h4>Total Admin</h4>
+                      </div>
+                      <div class="card-body">
+                        10
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+                  <div class="card card-statistic-1">
+                    <div class="card-icon bg-danger">
+                      <i class="far fa-newspaper"></i>
+                    </div>
+                    <div class="card-wrap">
+                      <div class="card-header">
+                        <h4>News</h4>
+                      </div>
+                      <div class="card-body">
+                        42
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+                  <div class="card card-statistic-1">
+                    <div class="card-icon bg-warning">
+                      <i class="far fa-file"></i>
+                    </div>
+                    <div class="card-wrap">
+                      <div class="card-header">
+                        <h4>Reports</h4>
+                      </div>
+                      <div class="card-body">
+                        1,201
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+                  <div class="card card-statistic-1">
+                    <div class="card-icon bg-success">
+                      <i class="fas fa-circle"></i>
+                    </div>
+                    <div class="card-wrap">
+                      <div class="card-header">
+                        <h4>Online Users</h4>
+                      </div>
+                      <div class="card-body">
+                        47
+                      </div>
+                    </div>
+                  </div>
+                </div>                  
+              </div>
         </div>
     </section>
+    <div class="col-md-8">
+        <div class="card">
+          <div class="card-header">
+            <h4>Invoices</h4>
+            <div class="card-header-action">
+              <a href="#" class="btn btn-danger">View More <i class="fas fa-chevron-right"></i></a>
+            </div>
+          </div>
+          <div class="card-body p-0">
+            <div class="table-responsive table-invoice">
+              <table class="table table-striped">
+                <tbody><tr>
+                  <th>Invoice ID</th>
+                  <th>Customer</th>
+                  <th>Status</th>
+                  <th>Due Date</th>
+                  <th>Action</th>
+                </tr>
+                <tr>
+                  <td><a href="#">INV-87239</a></td>
+                  <td class="font-weight-600">Kusnadi</td>
+                  <td><div class="badge badge-warning">Unpaid</div></td>
+                  <td>July 19, 2018</td>
+                  <td>
+                    <a href="#" class="btn btn-primary">Detail</a>
+                  </td>
+                </tr>
+                <tr>
+                  <td><a href="#">INV-48574</a></td>
+                  <td class="font-weight-600">Hasan Basri</td>
+                  <td><div class="badge badge-success">Paid</div></td>
+                  <td>July 21, 2018</td>
+                  <td>
+                    <a href="#" class="btn btn-primary">Detail</a>
+                  </td>
+                </tr>
+                <tr>
+                  <td><a href="#">INV-76824</a></td>
+                  <td class="font-weight-600">Muhamad Nuruzzaki</td>
+                  <td><div class="badge badge-warning">Unpaid</div></td>
+                  <td>July 22, 2018</td>
+                  <td>
+                    <a href="#" class="btn btn-primary">Detail</a>
+                  </td>
+                </tr>
+                <tr>
+                  <td><a href="#">INV-84990</a></td>
+                  <td class="font-weight-600">Agung Ardiansyah</td>
+                  <td><div class="badge badge-warning">Unpaid</div></td>
+                  <td>July 22, 2018</td>
+                  <td>
+                    <a href="#" class="btn btn-primary">Detail</a>
+                  </td>
+                </tr>
+                <tr>
+                  <td><a href="#">INV-87320</a></td>
+                  <td class="font-weight-600">Ardian Rahardiansyah</td>
+                  <td><div class="badge badge-success">Paid</div></td>
+                  <td>July 28, 2018</td>
+                  <td>
+                    <a href="#" class="btn btn-primary">Detail</a>
+                  </td>
+                </tr>
+              </tbody></table>
+            </div>
+          </div>
+        </div>
+      </div>
 @endsection
 
 @section('script')
