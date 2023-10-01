050f8ddd758b9cb92f39241d3d4a96cfadc5b264
adiwarsa <adiwarsa03@gmail.com>
2023-10-01 12:29:26 +0800

    add menu on sidebar

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -9,14 +9,19 @@
                         <li class="menu-header">Dashboard</li>
                         <li class="{{ Request::is('dashboard') ? 'active' : '' }}"><a class="nav-link" href="{{ url('/') }}"><i class="fas fa-fire"></i> <span>Dashboard</span></a></li>
                         @role('orangtua')
+                        <li class="menu-header">Nilai</li>
                         <li class="{{ Request::is('rapot*') ? 'active' : '' }}"><a class="nav-link" href="{{ route('ortu.santri') }}"><i class="fas fa-book"></i> <span>Rapot</span></a></li>
-                        <li class="{{ Request::is('jadwal*') ? 'active' : '' }}"><a class="nav-link" href="{{ url('/jadwal') }}"><i class="fas fa-book"></i> <span>Jadwal Pelajaran</span></a></li>
+                        <li class="menu-header">Pelajaran</li>
                         <li class="{{ Request::is('mapel*') ? 'active' : '' }}"><a class="nav-link" href="{{ url('/mapel') }}"><i class="fas fa-book"></i> <span>Mata Pelajaran</span></a></li>
+                        <li class="{{ Request::is('jadwal*') ? 'active' : '' }}"><a class="nav-link" href="{{ url('/jadwal') }}"><i class="fas fa-book"></i> <span>Jadwal Pelajaran</span></a></li>
                         @endrole
                         @role('guru')
-                            <li class="{{ Request::is('santri*') ? 'active' : '' }}"><a class="nav-link" href="{{ url('/santri') }}"><i class="fas fa-user"></i> <span>Santri</span></a></li>
-                            <li class="{{ Request::is('rapot*') ? 'active' : '' }}"><a class="nav-link" href="{{ url('/rapot') }}"><i class="fas fa-book"></i> <span>Rapot</span></a></li>
-                            <li class="{{ Request::is('mapel*') ? 'active' : '' }}"><a class="nav-link" href="{{ url('/mapel') }}"><i class="fas fa-book"></i> <span>Mata Pelajaran</span></a></li>
+                        <li class="menu-header">Santri</li>
+                        <li class="{{ Request::is('santri*') ? 'active' : '' }}"><a class="nav-link" href="{{ url('/santri') }}"><i class="fas fa-user"></i> <span>Santri</span></a></li>
+                        <li class="menu-header">Pelajaran</li>
+                        <li class="{{ Request::is('mapel*') ? 'active' : '' }}"><a class="nav-link" href="{{ url('/mapel') }}"><i class="fas fa-book"></i> <span>Mata Pelajaran</span></a></li>
+                        <li class="{{ Request::is('jadwal*') ? 'active' : '' }}"><a class="nav-link" href="{{ url('/jadwal') }}"><i class="fas fa-book"></i> <span>Jadwal Pelajaran</span></a></li>
+                        <li class="{{ Request::is('rapot*') ? 'active' : '' }}"><a class="nav-link" href="{{ url('/rapot') }}"><i class="fas fa-book"></i> <span>Rapot</span></a></li>
                         @endrole
                         @role('administrator')
                             <li class="menu-header"><i>Master</i></li>
@@ -27,6 +32,7 @@
                             <li class="{{ Request::is('jadwal*') ? 'active' : '' }}"><a class="nav-link" href="{{ url('/jadwal') }}"><i class="fas fa-book"></i> <span>Jadwal Pelajaran</span></a></li>
                             <li class="{{ Request::is('rapot*') ? 'active' : '' }}"><a class="nav-link" href="{{ url('/rapot') }}"><i class="fas fa-book"></i> <span>Rapot</span></a></li>
                             <li class="menu-header"><i>Auth</i></li>
+                            <li class="{{ Request::is('log*') ? 'active' : '' }}"><a class="nav-link" href="{{ url('/log') }}"><i class="fas fa-user-tag"></i> <span>Log</span></a></li>
                             <li class="{{ Request::is('permissions*') ? 'active' : '' }}"><a class="nav-link" href="{{ url('/permissions') }}"><i class="fas fa-key"></i> <span>Permissions</span></a></li>
                             <li class="{{ Request::is('roles*') ? 'active' : '' }}"><a class="nav-link" href="{{ url('/roles') }}"><i class="fas fa-user-tag"></i> <span>Roles</span></a></li>
                         @endrole
